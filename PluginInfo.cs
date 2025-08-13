742a4208e61fa231595d9295c392fd40eea27123
Abdhd-and-tbvr <abdullahlingokid@gmail.com>
2025-08-13 22:05:26 +0300

    all of my II Files

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.hd.gorillatag.menutemplate";
+        public const string Name = "HD'S Funny Menu";
+        public const string Description = "Made by @abdhd,﷽";
+        public const string Version = "1.0.1";
+    }
+}
